40947cf6d3ac637045e99a908adbcc0e92c10e9b
Veronika-c <veronikachornii2@gmail.com>
2026-03-05 14:15:43 -0500

    Add files via upload

---
+++ Virtual_store.py
@@ -0,0 +1,29 @@
+"""
+This program asks the user what they would like to buy and it tells you the user's purchases, total cost with taxs.
+"""
+
+#List of store items and price
+store_items = {"T-shirt", "sweatshirt", "keychain", "mug"}
+cart = []
+recipt = 0
+tax = 0
+
+#This tells the user what there is and ask what they would like to purchase
+for item in store_items:
+    item = (input("Welcome, what would you like to buy today?: "))
+    
+
+def store_items():
+    T-shirt == 11.50
+    sweatshirt == 20.00
+    keychain == 3.20
+    mug == 5.80
+    #if "T-shirt":
+        print("how many would you want")
+    if item not in store_items:
+        print("Sorry!, we do not have that, please go back")
+
+
+print(item)
+
+#total_cost = 0
